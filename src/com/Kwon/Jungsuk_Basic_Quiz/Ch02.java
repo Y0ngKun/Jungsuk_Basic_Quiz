a47633f5dccd5413a23da7a06b24f3944989b7d2
Y0ngKun <cksm00@naver.com>
2023-01-11 16:13:21 +0900

    Quiz

---
+++ src/com/Kwon/Jungsuk_Basic_Quiz/Ch02.java
@@ -0,0 +1,30 @@
+package com.Kwon.Jungsuk_Basic_Quiz;
+
+public class Ch02 {
+
+	public static void main(String[] args) {
+		
+		byte b =10;
+		char ch = 'A';
+		int i = 100;
+		long l = 1000L;
+		
+		
+		System.out.println('1'+2);
+		System.out.println('1'+'2');
+		System.out.println('J'+"AVA");
+
+		int x = 1;
+		int y = 2;
+		int z = 3;
+		
+		int tmp = x;
+		x = y;
+		y = z;
+		z = tmp;
+		
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+}
+}
---
+++ src/com/Kwon/Jungsuk_Basic_Quiz/Ch03.java
@@ -0,0 +1,9 @@
+package com.Kwon.Jungsuk_Basic_Quiz;
+
+public class Ch03 {
+
+	public static void main(String[] args) {
+
+	}
+
+}
